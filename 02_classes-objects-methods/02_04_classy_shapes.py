c3bce316534e291c90a55092b4f2b2d676e2600b
root <root@DESKTOP-G07NF43.localdomain>
2021-09-26 09:06:21 +0300

    Circule circumference and Rectangle perimter

--- 02_classes-objects-methods/02_04_classy_shapes.py
+++ 02_classes-objects-methods/02_04_classy_shapes.py
@@ -5,3 +5,39 @@
 # Write methods in the appropriate class so that you can calculate
 # the area of both the rectangle and the circle, the perimeter
 # of the rectangle, and the circumference of the circle.
+
+class Rectangle:
+    def __init__(self, length, width):
+        self.length = length
+        self.width = width
+
+    def __str__(self):
+        return f"{self.length},{self.width}"
+
+    def RectanglePerimeter(self):
+        perimeter = 2 * (self.length + self.width)
+        print(perimeter)
+
+class Circle:
+    def __init__(self, circleradius):
+        self.circleradius = circleradius
+
+    def circumference(self):
+        perimter = 2* 3.14 * self.circleradius
+        print(perimter)
+       
+
+    def __str__(self):
+        return f"{self.circleradius}"    
+
+rectangle1 = Rectangle(3,4)
+print(rectangle1)
+rectangle1.RectanglePerimeter()
+circle1 = Circle(2)
+print(circle1)
+circle1.circumference()
+
+
+
+
+    
