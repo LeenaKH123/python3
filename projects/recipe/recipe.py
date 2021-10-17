8e4cb1f4b98142b904f2750a769b490b4b3ed179
leenakhalili <engleena@yahoo.com>
2021-10-17 13:07:18 +0300

    recipe oop

--- projects/recipe/recipe.py
+++ projects/recipe/recipe.py
@@ -1,12 +1,16 @@
 class Ingredient:
-    def __init__( self, name):
+    def __init__( self, name, amount):
         self.name = name
+        self.amount = amount
 
     def expire(self):
         print(f"oops, these {self.name} went bad ...")
         self.name = "expired" + self.name
 
-i = Ingredient("peas")
+    def __str__(self):
+        return f"{self.name} ({self.amount})"
+
+i = Ingredient("peas", 2)
 print(i.name)  # OUTPUT: peas
 i.expire()
 print(i.name)  # OUTPUT: expired peas
