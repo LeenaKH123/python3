5d908fb4232f41611fbe7c8ed467adc378ef8fb7
leenakhalili <engleena@yahoo.com>
2021-10-17 13:12:25 +0300

    cooking in progress

--- projects/recipe/recipe.py
+++ projects/recipe/recipe.py
@@ -10,8 +10,10 @@ class Ingredient:
     def __str__(self):
         return f"{self.name} ({self.amount})"
 
+    def __repr__(self):
+        return f"Ingredient(name={self.name}, amount={self.amount})"
+
 i = Ingredient("peas", 2)
-print(i.name)  # OUTPUT: peas
-i.expire()
-print(i.name)  # OUTPUT: expired peas
-print(i.name)  # OUTPUT: expired peas
+print(i)
+print(repr(i))
+
